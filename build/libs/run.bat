71665437d1621022bc12f3dbe7583b5852d75b4f
Sergey Nikitin <kexa76@gmail.com>
2022-01-30 22:02:59 +0500

    clipboard listener implementation, no user input needed anymore

--- build/libs/run.bat
+++ build/libs/run.bat
@@ -1,1 +1,35 @@
+::[Bat To Exe Converter]
+::
+::fBE1pAF6MU+EWHreyHcjLQlHcA+HJGq0OpwY7enL/eWNp04JR95ue8LY1bqYJfIH71fYd4Mk2n5mlMIOHycJfV+oYQEirHxWs3bIZonM/jzPa3i6zmQEVmR4iy3ajTlb
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFBBRVAWMAE+1EbsQ5+n//NbVpUQOa+4wf4CV07eBQA==
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF65
+::cxAkpRVqdFKZSzk=
+::cBs/ulQjdF65
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJgZksALA==
+::ZQ05rAF9IBncCkqN+0xwdVsEAlTWXA==
+::ZQ05rAF9IAHYFVzEqQJhK1ZXQwqUOXmuA6d8
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFBBRVAWMAE+/Fb4I5/jHvunKoUITQuctbIrJl6LebrBHpED8cPY=
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
 java -jar 1c-converter-1.0-SNAPSHOT-all.jar
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,78 +1,84 @@
 package ru.kexan
 
 import java.awt.Toolkit
-import java.awt.datatransfer.Clipboard
-import java.awt.datatransfer.DataFlavor
+
+import java.awt.datatransfer.DataFlavor.stringFlavor
 import java.awt.datatransfer.StringSelection
-import java.awt.datatransfer.Transferable
+import java.awt.datatransfer.UnsupportedFlavorException
+import java.lang.Thread.sleep
+import java.sql.Timestamp
 import java.util.*
 
+
 fun main() {
-    while (true) {
-        val scanner = Scanner(System.`in`)
-        println("Выберите режим:\n1. Для клиентов\n2. Прайс\n3. Тест")
-        val mode = scanner.nextInt()
-        while (scanner.nextLine() != "end") {
-            println("Скопируй текст в буфер и нажми Enter для конвертации:")
-            scanner.nextLine()
-            val convertedText = convert(
-                Toolkit.getDefaultToolkit()
-                    .systemClipboard.getData(DataFlavor.stringFlavor) as String, mode
-            )
-            println(convertedText)
-            val clipboard: Clipboard = Toolkit.getDefaultToolkit().systemClipboard
-            val transferable: Transferable = StringSelection(convertedText)
-            clipboard.setContents(transferable, null)
-            println("Текст скопирован обратно в буфер. Нажми Enter для повторной конвертации или 'end' для выбора режима'")
+
+    val scanner = Scanner(System.`in`)
+    val clipboard = Toolkit.getDefaultToolkit().systemClipboard
+    var boolean = false
+
+    clipboard.addFlavorListener {
+        try {
+            boolean = !boolean
+            //FIXME два раза вызывается листенер т.к. в нем же инфа копируется в буфер, придумать фикс
+            //FIXME Можно попробовать сделать листенер на кей-комбо типа ctrl+c
+            if (boolean) {
+                val clipboardContent = convert(clipboard.getData(stringFlavor) as String)
+                sleep(200)
+                clipboard.setContents(StringSelection(clipboardContent), null)
+                println("\n $clipboardContent \n")
+            }
+        } catch (e: IllegalStateException) {
+            val timestamp = Timestamp(System.currentTimeMillis())
+            println("$timestamp Ошибка! Нет доступа к буферу обмена, пробую еще раз...")
+            return@addFlavorListener
+        } catch (e: UnsupportedFlavorException) {
+            val timestamp = Timestamp(System.currentTimeMillis())
+            println("$timestamp Ошибка! Неподдерживаемый тип данных, копируйте строки")
+            return@addFlavorListener
+        } catch (e: Exception) {
+            val timestamp = Timestamp(System.currentTimeMillis())
+            val text = clipboard.getData(stringFlavor) as String
+            println("$timestamp Неизвестная ошибка! Попробуйте еще раз. Запрос:")
+            println(text)
+            return@addFlavorListener
         }
     }
+
+    println("Программу можно свернуть, просто копируйте текст.")
+    while (true) {
+        scanner.nextLine()
+    }
+
 }
 
-fun convert(text: String, mode: Int): String {
-    val convertedText: List<String>
+fun convert(text: String): String {
 
-    when (mode) {
-        1 -> {
-            convertedText = text.replace("(1)", "")
-                .replace("(2)", "")
-                .replace("(3)", "")
-                .replace("(4)", "")
-                .replace("(5)", "")
-                .replace("(термопакет)", "")
-                .replace("(коробочка)", "")
-                .replace(",00", "")
-                .replace("шт", "-")
-                .split("\n")
-                .distinct()
-        }
-        2 -> {
-            convertedText = text
-                .replace("(1)", "")
-                .replace("(2)", "")
-                .replace("(3)", "")
-                .replace("(4)", "")
-                .replace("(5)", "")
-                .replace(",00", "")
-                .replace("шт", "-")
-                .split("\n")
-                .distinct()
-        }
-        else -> {
-            convertedText = text
-                .replace(",00", "")
-                .replace("шт", "-")
-                .replace(",0", " шт")
-                .split("\n")
-                .filter { it.contains("(1)") ||
-                        it.contains("(2)") ||
-                        it.contains("(3)") ||
-                        it.contains("(4)") }
-                .filterNot { it.contains("шт") }
-        }
+    val convertedText = text
+        .split("\n")
+        .toMutableList()
+
+    if (!text.contains("НФ-")) {
+        return text
     }
+
+    convertedText.forEachIndexed { index, string ->
+        convertedText[index] = string.substring(12)
+            .replace("(1)", "")
+            .replace("(2)", "")
+            .replace("(3)", "")
+            .replace("(4)", "")
+            .replace("(5)", "")
+            .replace("(термопакет)", "")
+            .replace("(коробочка)", "")
+            .replace(",00", "")
+            .replace(" шт", "-") //FIXME удаляет все проявления "шт", надо исправить
+            .plus(" р. \n")
+    }
+
     var formattedText = ""
-    for (string in convertedText) {
-        formattedText += if (mode != 3) "$string р.\n" else "$string\n"
+    for (string in convertedText.toMutableSet()) {
+        formattedText += string
     }
+
     return formattedText
 }
