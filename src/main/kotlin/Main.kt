6e712e5070d475d8502e19f37236a6fdce842219
Sergey Nikitin <kexa76@gmail.com>
2022-03-27 23:43:32 +0500

    potential fix

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,10 @@
 package ru.kexan
 
 import java.awt.Toolkit
-
+import java.awt.datatransfer.Clipboard
 import java.awt.datatransfer.DataFlavor.stringFlavor
 import java.awt.datatransfer.StringSelection
 import java.awt.datatransfer.UnsupportedFlavorException
-import java.lang.Thread.sleep
 import java.sql.Timestamp
 import java.util.*
 
@@ -14,45 +13,38 @@ fun main() {
     val scanner = Scanner(System.`in`)
     val clipboard = Toolkit.getDefaultToolkit().systemClipboard
     clipboard.addFlavorListener {
-        try {
-            val clipboardContent = clipboard.getData(stringFlavor) as String
-            if (!clipboardContent.contains("00-")) {
-                return@addFlavorListener
+        with(clipboard) {
+            try {
+                convertClipboardContents(this)
+            } catch (e: IllegalStateException) {
+                val timestamp = Timestamp(System.currentTimeMillis())
+                println("$timestamp Ошибка! Нет доступа к буферу обмена, пробую еще раз...")
+                convertClipboardContents(this)
+            } catch (e: UnsupportedFlavorException) {
+                val timestamp = Timestamp(System.currentTimeMillis())
+                println("$timestamp Ошибка! Неподдерживаемый тип данных, копируйте строки")
+                convertClipboardContents(this)
+            } catch (e: Exception) {
+                val timestamp = Timestamp(System.currentTimeMillis())
+                println("$timestamp Неизвестная ошибка! Попробуйте еще раз. Запрос:")
+                convertClipboardContents(this)
             }
-            val convertedContent = convert(clipboardContent)
-            sleep(200)
-            clipboard.setContents(StringSelection(convertedContent), null)
-            println("$convertedContent \n")
-        } catch (e: IllegalStateException) {
-            val timestamp = Timestamp(System.currentTimeMillis())
-            println("$timestamp Ошибка! Нет доступа к буферу обмена, пробую еще раз...")
-            return@addFlavorListener
-        } catch (e: UnsupportedFlavorException) {
-            val timestamp = Timestamp(System.currentTimeMillis())
-            println("$timestamp Ошибка! Неподдерживаемый тип данных, копируйте строки")
-            return@addFlavorListener
-        } catch (e: Exception) {
-            val timestamp = Timestamp(System.currentTimeMillis())
-            val text = clipboard.getData(stringFlavor) as String
-            println("$timestamp Неизвестная ошибка! Попробуйте еще раз. Запрос:")
-            println(text)
-            return@addFlavorListener
         }
     }
-
     println("Программу можно свернуть, просто копируйте текст.")
     while (true) {
         scanner.nextLine()
     }
-
 }
 
-fun convert(text: String): String {
-
-    val convertedText = text
+fun convertClipboardContents(clipboard: Clipboard): String {
+    val clipboardContent = clipboard.getData(stringFlavor) as String
+    if (!clipboardContent.contains("00-")) {
+        return clipboardContent
+    }
+    val convertedText = clipboardContent
         .split("\n")
         .toMutableList()
-
     convertedText.forEachIndexed { index, string ->
         convertedText[index] = string.substring(12)
             .replace("(1)", "")
@@ -66,11 +58,10 @@ fun convert(text: String): String {
             .replace("шт", "-") //FIXME удаляет все проявления "шт", надо исправить
             .plus(" р. \n")
     }
-
     var formattedText = ""
     for (string in convertedText.toMutableSet()) {
         formattedText += string
     }
-
+    clipboard.setContents(StringSelection(formattedText), null)
     return formattedText
 }
